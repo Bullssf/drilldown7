57615b73b80a967dcefea3fdba1d85959e453539
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-10-24 00:54:05 -0400

    Update WebpartTS with Consumer properties

--- src/webparts/cssChart/CssChartWebPart.ts
+++ src/webparts/cssChart/CssChartWebPart.ts
@@ -5,7 +5,23 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  BaseClientSideWebPart,
+
+  /**
+   * DD Subscriber: Step 0 - import from sp-dynamic-data
+   */
+  IWebPartPropertiesMetadata,
+
+  PropertyPaneDynamicFieldSet,
+  PropertyPaneDynamicField,
+  DynamicDataSharedDepth
+} from '@microsoft/sp-webpart-base';
+
+/**
+ * DD Subscriber: Step 0 - import from sp-dynamic-data
+ */
+import { DynamicProperty } from '@microsoft/sp-component-base';
 
 import * as strings from 'CssChartWebPartStrings';
 import CssChart from './components/CssChart';
@@ -13,23 +29,63 @@ import { ICssChartProps } from './components/ICssChartProps';
 
 export interface ICssChartWebPartProps {
   description: string;
+
+    /**
+   * DD Subscriber: Step 1 - add this.properties.cssChartProps to WebPartProps
+   */
+  cssChartProps: DynamicProperty<object>;
 }
 
 export default class CssChartWebPart extends BaseClientSideWebPart<ICssChartWebPartProps> {
 
-  public render(): void {
-    const element: React.ReactElement<ICssChartProps> = React.createElement(
-      CssChart,
-      {
-        description: this.properties.description
-      }
-    );
+  protected onInit(): Promise<void> {
 
-    ReactDom.render(element, this.domElement);
+    /**
+     * DD Subscriber: Step 5 - (7:33) Check to see if this was wired up 
+     */
+    if ( !this.properties.cssChartProps.reference ) {
+        this.properties.cssChartProps.setValue({ title: 'propsNotDefined', id: 'NA' });
+    }
+  
+     return Promise.resolve();
+  
+    }
+
+  public render(): void {
+
+       /**
+   * DD Subscriber: Step 6 - (8:33) Check to see if this was wired up 
+   */
+  const pickedProps : any | undefined = this.properties.cssChartProps.tryGetValue();
+
+  /**
+   * DD Subscriber: Step 7 - (8:33) Only if props were set, render component 
+   */
+    if ( pickedProps ) {
+
+      const element: React.ReactElement<ICssChartProps> = React.createElement(
+        CssChart,
+        {
+          description: this.properties.description
+        }
+      );
+
+      ReactDom.render(element, this.domElement);
+    }
   }
 
   protected onDispose(): void {
     ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+    
+  /**
+   * DD Subscriber: Step 3 - add this.properties.cssChartProps to WebPartProps
+   */
+  protected get propertiesMetadata(): IWebPartPropertiesMetadata {
+    return {
+      'cssChartProps': { dynamicPropertyType: 'object' }
+    };
   }
 
   protected get dataVersion(): Version {
@@ -40,13 +96,38 @@ export default class CssChartWebPart extends BaseClientSideWebPart<ICssChartWebP
     return {
       pages: [
         {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
+
+  /**
+   * DD Subscriber: Step 4 - add options to PropertyPane
+   */
+                PropertyPaneDynamicFieldSet({
+                  label: 'Pick cssChartProps Source',
+                  fields: [
+                    PropertyPaneDynamicField('cssChartProps', {
+                      label: 'Test Source',
+
+                     })
+                  ],
+   /**
+   * DD Subscriber: Step 5 - ( 10: 45 ) :  sharedConfiguration in case you don't want
+   * settings on consumer:
+   *    Depth:  If you have multiple dynamic properities, you can specify how the connection is shared
+   *    depth: DynamicDataSharedDepth.Property, === all consumers can share the property 
+   *    depth: DynamicDataSharedDepth.None, == entire object
+   */
+                  sharedConfiguration: {
+                    depth: DynamicDataSharedDepth.None,
+
+                    source: {
+                      sourcesLabel: 'Select webpart containing your source'
+                    }
+                    /*                    */
+                  }
+                }),
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
                 })
