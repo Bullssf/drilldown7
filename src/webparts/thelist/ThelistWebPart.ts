4ec495f7e77f5f5c97c380b605186b7e19c168d7
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-10-26 16:49:12 -0400

    Add theList webpart

---
+++ src/webparts/thelist/ThelistWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ThelistWebPartStrings';
+import Thelist from './components/Thelist';
+import { IThelistProps } from './components/IThelistProps';
+
+export interface IThelistWebPartProps {
+  description: string;
+}
+
+export default class ThelistWebPart extends BaseClientSideWebPart<IThelistWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IThelistProps> = React.createElement(
+      Thelist,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/thelist/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IThelistWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ThelistWebPartStrings' {
+  const strings: IThelistWebPartStrings;
+  export = strings;
+}
