3705b92180c547f7d5be73150703c2aaad84325c
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-03-19 01:53:43 -0400

    Update About.tsx

--- src/webparts/drilldown7/components/HelpPanel/Content/About.tsx
+++ src/webparts/drilldown7/components/HelpPanel/Content/About.tsx
@@ -19,8 +19,9 @@ export function aboutTable( repoLinks: IRepoLinks ) {
         rows: [],
     };
 
-    table.rows.push( createAboutRow('2022-03-1?','1.2.0.3',<span>#85, #86 - Add Banner with Error Messages</span>, repoLinks ) );
-    table.rows.push( createAboutRow('2022-03-1?','1.2.0.3',<span>#82, #83 - YY-MM Refiners, #76 - Hide Folders option</span>, repoLinks ) );
+    table.rows.push( createAboutRow('2022-03-1?','1.2.0.3'  ,<span>#88, Performance improvements</span>, repoLinks ) );
+    table.rows.push( createAboutRow('"','"'                 ,<span>#85, #86 - Add Banner with Error Messages</span>, repoLinks ) );
+    table.rows.push( createAboutRow('"','"'                 ,<span>#82, #83 - YY-MM Refiners, #76 - Hide Folders option</span>, repoLinks ) );
 
     table.rows.push( createAboutRow('2022-03-18','1.2.0.2',<span>Espanol language update, remove settings alerts!</span>, repoLinks ) );
 
