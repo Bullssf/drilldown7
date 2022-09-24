75282ccdb314c6318aa00d42abb5f5368883348a
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-09-23 23:21:24 -0400

    v2.0.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,29 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+//Added for webpack-analyzer VVVVVVVVVVVVVVVV
+const path = require('path');
+const bundleAnalyzer = require('webpack-bundle-analyzer');
+
+build.configureWebpack.mergeConfig({
+  additionalConfiguration: (generatedConfiguration) => {
+    const lastDirName = path.basename(__dirname);
+    const dropPath = path.join(__dirname, 'temp', 'stats');
+    generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+      generateStatsFile: true,
+      statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+      logLevel: 'error'
+    }));
+
+    return generatedConfiguration;
+  }
+});
+
+//Added for webpack-analyzer ^^^^^^^^^^^^^^^^^
+
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
   var result = getTasks.call(build.rig);
